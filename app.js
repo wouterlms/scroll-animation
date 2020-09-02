0ccd52264d1d9fa13b4a9afff054e83ff6c7a784
Wouter Laermans <wouter.laermans@gmail.com>
2020-09-02 16:53:51 +0200

    example updated

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const example = new Aos.Scene({
 	el: '.element',
 	trigger: '.trigger',
 	pinned: true,
-	debug: true,
+	// debug: true,
 	triggerPosition: .2,
 });
 
 example
 	.animate('rotate', {
-		to: 20
+		to: 20,
+		end: 25,
 	})
+	.animate('translateX', {
+		from: 500,
+		to: 0,
+		start: 25,
+		end: 75,
+	})
+	.animate('rotate', {
+		from: 20,
+		to: 0,
+		start: 75
+	});
 
 
 new Aos.Controller()
